9f358aaf0c2a8d71d372b40ea7eb5816cd943eff
AlphaAnimal <h.h.redondo@gmail.com>
2022-04-22 15:37:01 +0200

    only missing score tracking for the test

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -7,50 +7,71 @@ const Test: React.FC<{}> = () => {
   const words = ['some', 'random', 'words', 'which', 'the', 'user', 'has', 'to', 'type', 'but', 'they', 'should', 'come', 'from', 'api']
   const wordsString = words.join(' ')
 
+  const [value, setValue] = useState<string>('')
   const [currentWords, setCurrentWords] = useState<string>(wordsString)
+  const [currentDynamicWord, setCurrentDynamicWord] = useState<string>(currentWords.substring(0, currentWords.indexOf(' ')))
   const [currentWord, setCurrentWord] = useState<string>(currentWords.substring(0, currentWords.indexOf(' ')))
-  const [currentLetter, setCurrentLetter] = useState<string>(currentWord.charAt(0))
+  const [reverseWord, setReverseWord] = useState<string>('')
+  const [currentLetter, setCurrentLetter] = useState<string>(currentDynamicWord.charAt(0))
   const [errorMade, setErrorMade] = useState<boolean>(false)
   const [score, setScore] = useState<number>(0)
-
-
 
   useEffect(() => {
     setCurrentLetter(currentWords.charAt(0))
   },[currentWords])
 
   useEffect(() => {
-    console.log('Current word: ', currentWord)
-  },[currentWord])
+    console.log('Current word: ', currentDynamicWord)
+  },[currentDynamicWord])
+  useEffect(() => {
+    console.log('Reverse word: ', reverseWord)
+  },[reverseWord])
   useEffect(() => {
     console.log('Current words: ', currentWords)
   },[currentWords])
 
+
   const nextWord = () => {
-    // problem is here
-    // const newWords = removeFirstWord(wordsString);
-    // setCurrentWords(newWords);
-    const nextWords = currentWords.slice(1);
-    setCurrentWords(nextWords);
-    setCurrentWord(nextWords.substring(0, nextWords.indexOf(' ')));
+    const trimmedWords = currentWords.trim()   
+    const nextWords = trimmedWords.slice(currentWords.indexOf(' '));
+    setCurrentWords(nextWords.trim());
+    const trimtrim = nextWords.trim()
+    setCurrentDynamicWord(trimtrim.slice(0, trimtrim.indexOf(' ')));
+    setCurrentWord(trimtrim.slice(0, trimtrim.indexOf(' ')));
+    setReverseWord('');
   }
 
   const handleKeyDown = (e: any) => {
-    //console.log('currentWords: ', currentWords)
-
     if (e.keyCode === 32){
-      setCurrentWords(currentWords.slice(1));
       nextWord();
+      setValue('')
       console.log('value entered: spacebar');
     }
+    else if (e.keyCode === 8){
+      console.log('value entered: backspace');
+      if (reverseWord.length > 0){
+        const currentChar = reverseWord.length -1;
+        const currentWordArray = currentWord.split('');
+        const letterToAdd = currentWordArray[currentChar]
+        setCurrentWords(letterToAdd + currentWords)
+        setCurrentDynamicWord(letterToAdd + currentDynamicWord)
+        setReverseWord(reverseWord.slice(0, -1))
+      }
+      else if (reverseWord.length == 0){
+        console.log('No chars to delete')
+      }
+    }
     else {
-      if (currentWord.length > 0){
+      setValue(e.value)
+      if (currentDynamicWord.length > 0){
         console.log('value entered: ', e.key);
-        setCurrentWord(currentWord.slice(1));
+        setCurrentDynamicWord(currentDynamicWord.slice(1));
+        const letterGone = currentDynamicWord.slice(0, 1);
+        setReverseWord(reverseWord+letterGone)
         setCurrentWords(currentWords.slice(1));
          e.key !== currentLetter ? setErrorMade(true) : setErrorMade(false)
       }
-      else if (currentWord.length === 0){
+      else if (currentDynamicWord.length === 0){
         console.log('Word finished')
         if (errorMade === false){
           setScore(score + 1)
@@ -61,7 +82,7 @@ const Test: React.FC<{}> = () => {
 
   return (
     <Stack direction="row" spacing="2">
-      <TextField id="standard-basic" variant="standard" style={{maxWidth: '7px'}} onKeyDown={(e) => handleKeyDown(e)} />
+      <TextField id="standard-basic" variant="standard" style={{maxWidth: '7px'}} onKeyDown={(e) => handleKeyDown(e)} value={value} />
       <TextField disabled={true} variant="standard" value={currentWords}/>
     </Stack>
   );
